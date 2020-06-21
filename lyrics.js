fd630a64569fc7da69d405b97ee5d05eac1e9c68
iiiwelter <67241687+iiiwelter@users.noreply.github.com>
2020-06-21 23:50:46 +0300

    Create lyrics.js

---
+++ lyrics.js
@@ -0,0 +1,102 @@
+const { Command } = require('discord.js-commando');
+const { MessageEmbed } = require('discord.js');
+const fetch = require('node-fetch');
+const cheerio = require('cheerio');
+const { geniusLyricsAPI } = require('../../config.json');
+
+module.exports = class LyricsCommand extends Command {
+  constructor(client) {
+    super(client, {
+      name: 'كلمات',
+      memberName: 'كلمات',
+      description:
+        'كلمات الاغاني',
+      group: 'music',
+      throttling: {
+        usages: 1,
+        duration: 5
+      },
+      args: [
+        {
+          key: 'songName',
+          default: '',
+          type: 'string',
+          prompt: 'كلمات اي اغنية تبي ؟'
+        }
+      ]
+    });
+  }
+  async run(message, { songName }) {
+    if (
+      songName == '' &&
+      message.guild.musicData.isPlaying &&
+      !message.guild.triviaData.isTriviaRunning
+    ) {
+      songName = message.guild.musicData.nowPlaying.title;
+    } else if (songName == '' && message.guild.triviaData.isTriviaRunning) {
+      return message.say('Please try again after the trivia has ended');
+    } else if (songName == '' && !message.guild.musicData.isPlaying) {
+      return message.say(
+        'ما فيه اغنية تشتغل الحين , حاول مره ثانية'
+      );
+    }
+    const sentMessage = await message.channel.send(
+      '👀 جاااري البححث 👀'
+    );
+
+    // get song id
+    var url = `https://api.genius.com/search?q=${encodeURI(songName)}`;
+
+    const headers = {
+      Authorization: `Bearer ${geniusLyricsAPI}`
+    };
+    try {
+      var body = await fetch(url, { headers });
+      var result = await body.json();
+      const songID = result.response.hits[0].result.id;
+
+      // get lyrics
+      url = `https://api.genius.com/songs/${songID}`;
+      body = await fetch(url, { headers });
+      result = await body.json();
+
+      const song = result.response.song;
+
+      let lyrics = await getLyrics(song.url);
+      lyrics = lyrics.replace(/(\[.+\])/g, '');
+
+      if (lyrics.length > 4095)
+        return message.say('الكلمات طوييله يبوو');
+      if (lyrics.length < 2048) {
+        const lyricsEmbed = new MessageEmbed()
+          .setColor('#00724E')
+          .setDescription(lyrics.trim());
+        return sentMessage.edit('', lyricsEmbed);
+      } else {
+        // lyrics.length > 2048
+        const firstLyricsEmbed = new MessageEmbed()
+          .setColor('#00724E')
+          .setDescription(lyrics.slice(0, 2048));
+        const secondLyricsEmbed = new MessageEmbed()
+          .setColor('#00724E')
+          .setDescription(lyrics.slice(2048, lyrics.length));
+        sentMessage.edit('', firstLyricsEmbed);
+        message.channel.send(secondLyricsEmbed);
+        return;
+      }
+    } catch (e) {
+      console.error(e);
+      return sentMessage.edit(
+        'حصل خطأ , حاول مره ثانية'
+      );
+    }
+    async function getLyrics(url) {
+      const response = await fetch(url);
+      const text = await response.text();
+      const $ = cheerio.load(text);
+      return $('.lyrics')
+        .text()
+        .trim();
+    }
+  }
+};
